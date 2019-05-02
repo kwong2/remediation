591298e8bd6b9a2b61c301c9cb2ce645d4dedcea
Kevin Wong <k.wong4@yahoo.com>
2019-05-02 16:10:06 -0400

    changed xml to json

--- cloudcheckr.py
+++ cloudcheckr.py
@@ -6,7 +6,8 @@ Created on Wed May  1 17:42:46 2019
 """
 import requests
 import pandas as pd
-import xml.etree.ElementTree as ET 
+# import xml.etree.ElementTree as ET 
+import json
 import re
 
 pd.set_option('display.max_rows', 500)
@@ -17,26 +18,26 @@ env = "https://api.cloudcheckr.com"
 account_name = input("Enter account name: ")
 admin_api_key = input("Enter api key: ")
 
-api_url = env + "/api/inventory.xml/get_resources_ec2_details_v4?access_key=" + admin_api_key + "&use_account=" + account_name
+api_url = env + "/api/inventory.json/get_resources_ec2_details_v4?access_key=" + admin_api_key + "&use_account=" + account_name
 
 r = requests.get(api_url)
+theJson = r.text
+parsedJson=json.loads(theJson)
 
-xmlstring = r.text
-xmlstring = re.sub(' xmlns="[^"]+"', '', xmlstring, count=1)
-root = ET.fromstring(xmlstring)
+print(json.dumps(parsedJson, indent=2, sort_keys=True))
 
-list = []
+# list = []
 
-for instance in root.findall('./Ec2Instances/Ec2InstanceV4'):
-    instanceid = instance.find('InstanceId').text  
-    for tag in instance.iter('ResourceTag'):
-        key = tag.find('Key')
-        key = key.text
-        value = tag.find('Value')
-        value = value.text
+# for instance in root.findall('./Ec2Instances/Ec2InstanceV4'):
+#     instanceid = instance.find('InstanceId').text  
+#     for tag in instance.iter('ResourceTag'):
+#         key = tag.find('Key')
+#         # key = key.text
+#         value = tag.find('Value')
+#         # value = value.text
         
-        thisdict = dict(instanceid=instanceid, tag=key, value=value)
-        list.append(thisdict)
+#         thisdict = dict(instanceid=instanceid, tag=key, value=value)
+#         list.append(thisdict)
         
-df = pd.DataFrame.from_dict(list)
-print(df)
+# df = pd.DataFrame.from_dict(list)
+# print(df)
